9ff0821d298393cab12a7daa4470c10b3932dd59
dbmvz18 <49908771+dbmvz18@users.noreply.github.com>
2019-11-01 17:56:11 +0100

    neue html-templates erstellt, bootstrap eingebunden, html-blöcke erstellt (footer, header), nav-bar erstellt, main.py aktualisiert

--- foodmanager/main.py
+++ foodmanager/main.py
@@ -1,17 +1,20 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, request, redirect, flash
 
 
 app = Flask("Hello World")
 
 
+#Beispiel Augabe
 @app.route('/hello_simple')
 def hello_world_simple():
 	return 'Hello, you r the best!'
 
-@app.route('/hello')
+#Ausgabe/Verlinkung zu html-template
+@app.route('/index')
 def hello_world():
-    return render_template('index.html', name="Fabian")
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
